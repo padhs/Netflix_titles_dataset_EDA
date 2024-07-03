81b5c6298effd1fb4f03db0e87ca63146319ca1e
padhs <arosekpadhi@gmail.com>
2024-07-04 03:05:13 +0530

    preprocessing

--- netflix_EDA.py
+++ netflix_EDA.py
@@ -7,11 +7,24 @@ import seaborn as sns
 import plotly.express as px
 
 # load the dataset
-df = pd.read_csv('./dataset/netflix_titles.csv'
-                 , sep=',')
+df = pd.read_csv('./dataset/netflix_titles.csv')
 print(df.head())
 print(df.info())
 print(df.shape)
 
 # there are some nulls
 print(df.columns)
+print(df.isnull().sum())
+
+# The max null counts are in directors, cast and country of origin
+# workflow: can't change the nulls in director, cast, country & date_added. It will be misleading. better to leave them as nulls
+# workflow2: random sampling for ratings and duration or we can remove those records
+
+# visualizing missing values:
+sns.heatmap(df.isnull())
+plt.show()
+
+# shows the distribution of data
+sns.histplot(df)
+plt.show()
+
